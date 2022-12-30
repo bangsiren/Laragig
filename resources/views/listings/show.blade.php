563b4a190fb8db36750e779f64b4e8b27b878177
bangsiren <bangsir61@gmail.com>
2022-12-30 05:00:54 -1100

    updates

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -8,14 +8,13 @@
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src={{$listing->logo ? asset('storage/' . $listing->logo) :
                 asset("images/download.png")}} alt="" />
-
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                 <x-listing-tags :tagsCsv="$listing->tags" />
                 <div class="text-lg my-4">
                     <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
                 </div>
-                
+
                 <div class="border border-gray-200 w-full mb-6"></div>
                 <div>
                     <h3 class="text-3xl font-bold mb-4">
